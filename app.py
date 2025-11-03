add152989f3e01177776880e9fe9b0d855be36c3
lbv_o <gromlbv@gmail.com>
2025-11-03 21:42:49 +0300

    added bf

--- app.py
+++ app.py
@@ -1,9 +1,19 @@
-from flask import Flask, render_template, send_from_directory
+from flask import Flask, render_template, send_from_directory, request
 
 app = Flask(__name__, static_folder='static', template_folder='templates')
 
+@app.before_request
+def before_request():
+    host = request.host.lower()
+    if host.startswith('bf.') or host == 'bf':
+        request.subdomain = 'bf'
+    else:
+        request.subdomain = None
+
 @app.route('/')
 def index():
+    if hasattr(request, 'subdomain') and request.subdomain == 'bf':
+        return render_template('bf.j2')
     return render_template('index.html')
 
 @app.route('/utils/<path:filename>')
